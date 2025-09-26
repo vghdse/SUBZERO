97f9aa9a8e6fff27d197389b337e1e311b414f30
vghdse <h.hanayan66@gmail.com>
2025-09-26 20:30:24 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 module.exports = {
   SESSION_ID: "SUBZERO~b516c04a833f2080eaf9e9ff23b1689164372281", // Put Session Id Here Inside "*"
   OWNER_NUMBER: "263719647303", // Put Owner Number Here 
-  PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
+  PREFIX: ",", // Default Prefix You Can Change To .,*!?+-&%#@
   CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
 };
 
